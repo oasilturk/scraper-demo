e98833d620b5994939ea3329706966baa9e952a4
oasilturk <moasilturk@gmail.com>
2024-02-25 18:24:40 +0300

    use all classes in main for inserting products

--- app/main.py
+++ app/main.py
@@ -1,11 +1,22 @@
-import db
+import os
+import config
+from xml_parser import XMLParser
+from product import Product
+from database import Database
+
 
 def main():
-    try:
-        result = db.insert_test_data()
-        print(f"Inserted data with ID: {result}")
-    except Exception as e:
-        print(f"An error occurred: {e}")
+    db = Database()
+    collection = db.get_collection()
+
+    file_path = os.path.join(config.APP_ABS_PATH, "..", "lonca-sample.xml")
+    xml_parser = XMLParser(file_path)
+    xml_elements = xml_parser.parse()
+    for xml_element in xml_elements:
+        product = Product.map_from_xml_element(xml_element)
+        product.format()
+        product.insert(collection)
+
 
 if __name__ == "__main__":
     main()
