6782e534ec24cb9ad58e48a0bc2b78a3fe710e3e
oasilturk <moasilturk@gmail.com>
2024-02-25 16:20:01 +0300

    add app's absolute path to config

--- app/sample_config.py
+++ app/sample_config.py
@@ -1,3 +1,8 @@
-CONN_URI = "mongodb://<your_db_user>:<your_db_password>@localhost:27017/<your_database_name>"
+import os
+
+CONN_URI = (
+    "mongodb://<your_db_user>:<your_db_password>@localhost:27017/<your_database_name>"
+)
 DB_NAME = "scraper_demo_db"
 COLLECTION_NAME = "products"
+APP_ABS_PATH = os.path.dirname(os.path.abspath(__file__))
