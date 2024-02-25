3191fc563957522358b0b17a75298911ed9900ef
oasilturk <moasilturk@gmail.com>
2024-02-25 15:43:42 +0300

    make use of config file for db conn

--- app/db.py
+++ app/db.py
@@ -1,13 +1,11 @@
 from pymongo import MongoClient
+import config
 
-MONGO_URI = "mongodb://localhost:27017"
-DATABASE_NAME = "test_database"
-
-client = MongoClient(MONGO_URI)
-db = client[DATABASE_NAME]
+client = MongoClient(config.CONN_URI)
+db = client[config.DB_NAME]
+collection = db[config.COLLECTION_NAME]
 
 def insert_test_data():
-    collection = db["test_collection"]
     test_data = {"name": "test", "value": 123}
     result = collection.insert_one(test_data)
     return result.inserted_id
